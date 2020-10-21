b08edfada276e2b95b69c4c917d629ac982260be
Yui <yunistar@icloud.com>
2020-10-21 16:59:29 +0200

    xref64 + format

--- sephelper.py
+++ sephelper.py
@@ -5,11 +5,23 @@ import ida_segment
 import ida_bytes
 import ida_funcs
 
+def xref64(ea, name):
+  ea_list = list(idautils.XrefsTo(ea))
+
+  if ea_list[0].frm != ida_idaapi.BADADDR:
+  func_ea = ida_funcs.get_func(ea_list[0].frm).start_ea
+    print("  [sephelper]: %s = 0x%x" % (name, func_ea))
+    idc.set_name(func_ea, name, idc.SN_CHECK)
+    return func_ea
+
+  print("  [sephelper]: %s = NULL" % name)
+  return ida_idaapi.BADADDR
+
 def function64(base_ea, base_end_ea, name, sequence):
   seq_ea = ida_search.find_binary(base_ea, base_end_ea, sequence, 0x10, ida_search.SEARCH_DOWN)
 
   if seq_ea != ida_idaapi.BADADDR:
-    func = idaapi.get_func(seq_ea)
+  func = idaapi.get_func(seq_ea)
     print("  [sephelper]: %s = 0x%x" % (name, func.start_ea))
     idc.set_name(func.start_ea, name, idc.SN_CHECK)
     return func.start_ea
@@ -18,47 +30,47 @@ def function64(base_ea, base_end_ea, name, sequence):
   return ida_idaapi.BADADDR
 
 def find_function(seg_start, seg_end):
-  function = [
-    ("_bzero", "63 e4 7a 92  42 00 00 8b"),
-    ("_ccn_cmp", "7f 00 05 eb  c0 80 80 9a"),
-    ("_ccn_sub", "84 00 04 eb  40 00 00 b5"),
-    ("_reload_cache", "1f 87 08 d5"),
-    ("_DEROiCompare", "a1 01 00 b4  02 05 40 f9"),
-    ("_DERParseInteger", "00 01 00 35  e8 07 40 f9"),
-    ("_verify_pkcs1_sig", "68 0e 00 54  a1 12 40 f9"),
-    ("_DERParseSequence", "e0 01 00 35  e8 07 40 f9"),
-    ("_DERImg4DecodePayload", "33 03 00 b4  09 01 40 f9"),
-    ("_Img4DecodeGetPayload", "00 81 c9 3c  20 00 80 3d"),
-    ("_verify_chain_signatures", "?? 09 00 b4  68 12 40 f9"),
-    ("_DERImg4DecodeFindInSequence", "60 02 80 3d  fd 7b 44 a9"),
-    ("_DERDecodeItemPartialBufferGetLength", "09 04 40 f9  3f 09 00 f1"),
-    ("_Img4DecodeEvaluateDictionaryProperties", "e0 03 1f 32  0a fd 7e d3"),
-    ("_DERImg4DecodeParseManifestProperties", "80 02 80 3d  a1 3a 00 91"),
-    ("_Img4DecodeGetPropertyBoolean", "21 08 43 b2  e0 03 00 91"),
-    ("_Img4DecodeCopyPayloadDigest", "?? ?? 02 91  e0 03 15 aa"),
-    ("__Img4DecodeGetPropertyData", "00 00 80 52  e8 17 40 f9"),
-    ("_DERImg4DecodeFindProperty", "00 00 80 52  a8 0a 43 b2"),
-    ("_DERDecodeSeqContentInit", "09 04 40 f9  08 01 09 8b"),
-    ("_DERImg4DecodeProperty", "e8 07 40 b9  08 09 43 b2"),
-    ("_DERParseBitString", "08 00 80 d2  5f 00 00 39"),
-    ("_boot_check_panic", "49 00 c0 d2  09 21 a8 f2"),
-    ("_DERDecodeSeqNext", "e8 03 00 f9  28 01 08 cb"),
-    ("_ccdigest_update", "e1 00 00 54  81 fe 46 d3"),
-    ("_DERParseBoolean", "08 01 40 39  1f fd 03 71"),
-    ("_Img4DecodeInit", "20 01 00 35  c0 c2 00 91"),
-    ("_ccdigest_init", "f4 03 00 aa  60 22 00 91"),
-    ("_DERImg4Decode", "61 03 00 54  88 26 40 a9"),
-    ("__parse_chain", "5a 3d 00 12  77 3d 00 12"),
-    ("_image4_load", "14 00 00 14  a0 6c A7 52"),
-    ("_cchmac_init", "69 22 00 91  8a 0b 80 52"),
-    ("_ccn_add", "84 00 00 b1  40 00 00 b5"),
-    ("_cc_muxp", "08 c1 20 cb  28 00 08 8a"),
-    ("_memcpy", "63 80 00 91  63 e8 7b 92"),
-    ("_ccn_n", "63 04 00 91  5f 00 00 f1"),
-  ]
+  function64(seg_start, seg_end, "_DEROiCompare", "a1 01 00 b4  02 05 40 f9")
+  function64(seg_start, seg_end, "_DERImg4Decode", "61 03 00 54  88 26 40 a9")
+  function64(seg_start, seg_end, "_DERParseBoolean", "08 01 40 39  1f fd 03 71")
+  function64(seg_start, seg_end, "_DERParseInteger", "00 01 00 35  e8 07 40 f9")
+  function64(seg_start, seg_end, "_DERParseSequence", "e0 01 00 35  e8 07 40 f9")
+  function64(seg_start, seg_end, "_DERDecodeSeqNext", "e8 03 00 f9  28 01 08 cb")
+  function64(seg_start, seg_end, "_DERParseBitString", "08 00 80 d2  5f 00 00 39")
+  function64(seg_start, seg_end, "_DERImg4DecodePayload", "33 03 00 b4  09 01 40 f9")
+  function64(seg_start, seg_end, "_DERImg4DecodeProperty", "e8 07 40 b9  08 09 43 b2")
+  function64(seg_start, seg_end, "_DERDecodeSeqContentInit", "09 04 40 f9  08 01 09 8b")
+  function64(seg_start, seg_end, "_DERImg4DecodeFindProperty", "00 00 80 52  a8 0a 43 b2")
+  function64(seg_start, seg_end, "_DERImg4DecodeFindInSequence", "60 02 80 3d  fd 7b 44 a9")
+  function64(seg_start, seg_end, "_DERDecodeItemPartialBufferGetLength", "09 04 40 f9  3f 09 00 f1")
+  function64(seg_start, seg_end, "_DERImg4DecodeParseManifestProperties", "80 02 80 3d  a1 3a 00 91")
+    
+  function64(seg_start, seg_end, "_Img4DecodeInit", "20 01 00 35  c0 c2 00 91")
+  start_func = function64(seg_start, seg_end, "_Img4DecodeGetPayload", "00 81 c9 3c  20 00 80 3d")
+  function64(seg_start, seg_end, "_Img4DecodeGetPropertyData", "00 00 80 52  e8 17 40 f9")
+  function64(seg_start, seg_end, "_Img4DecodeCopyPayloadDigest", "?? ?? 02 91  e0 03 15 aa")
+  function64(seg_start, seg_end, "_Img4DecodeGetPropertyBoolean", "21 08 43 b2  e0 03 00 91")
+  function64(seg_start, seg_end, "_Img4DecodeEvaluateDictionaryProperties", "e0 03 1f 32  0a fd 7e d3")
+    
+  function64(seg_start, seg_end, "_ccn_n", "63 04 00 91  5f 00 00 f1")
+  function64(seg_start, seg_end, "_ccn_cmp", "7f 00 05 eb  c0 80 80 9a")
+  function64(seg_start, seg_end, "_ccn_sub", "84 00 04 eb  40 00 00 b5")
+  function64(seg_start, seg_end, "_ccn_add", "84 00 00 b1  40 00 00 b5")
+  function64(seg_start, seg_end, "_cc_muxp", "08 c1 20 cb  28 00 08 8a")
+  function64(seg_start, seg_end, "_cchmac_init", "69 22 00 91  8a 0b 80 52")
+  function64(seg_start, seg_end, "_ccdigest_update", "e1 00 00 54  81 fe 46 d3")
+  function64(seg_start, seg_end, "_ccdigest_init", "f4 03 00 aa  60 22 00 91")
 
-  for (name_ea, seq_ea) in function:
-    function64(seg_start, seg_end, name_ea, seq_ea)
+    xref64(start_func, "_image4_load")
+  function64(seg_start, seg_end, "_bzero", "63 e4 7a 92  42 00 00 8b")
+  function64(seg_start, seg_end, "_memcpy", "63 80 00 91  63 e8 7b 92")
+  function64(seg_start, seg_end, "__parse_chain", "5a 3d 00 12  77 3d 00 12")
+  function64(seg_start, seg_end, "_reload_cache", "1f 87 08 d5")
+  function64(seg_start, seg_end, "_boot_check_panic", "49 00 c0 d2  09 21 a8 f2")
+  function64(seg_start, seg_end, "_verify_pkcs1_sig", "68 0e 00 54  a1 12 40 f9")
+  function64(seg_start, seg_end, "_verify_chain_signatures", "?? 09 00 b4  68 12 40 f9")
+
+  function64(seg_start, seg_end, name_ea, seq_ea)
 
 def accept_file(fd, fname):
   ret = 0
