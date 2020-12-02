d1d2733c3cbc7bafcb6571e49a328f562ee11a04
Yui <yunistar@icloud.com>
2020-12-02 13:14:27 +0100

    Fixed a problem with function64()
    It apparently took 2 arguments...

--- sephelper.py
+++ sephelper.py
@@ -5,7 +5,7 @@ import ida_segment
 import ida_bytes
 import ida_funcs
 
-def function64(base_ea, base_end_ea, name, sequence):
+def func64(base_ea, base_end_ea, name, sequence):
   seq_ea = ida_search.find_binary(base_ea, base_end_ea, sequence, 0x10, ida_search.SEARCH_DOWN)
 
   if seq_ea != ida_idaapi.BADADDR:
@@ -22,50 +22,50 @@ def function64(base_ea, base_end_ea, name, sequence):
 # https://gist.github.com/bazad/42054285391c6e0dcd0ede4b5f969ad2
 
 def find_function(seg_start, seg_end):
-  function64(seg_start, seg_end, "_DEROidCompare", "a1 01 00 b4  02 05 40 f9")
-  function64(seg_start, seg_end, "_DERImg4Decode", "61 03 00 54  88 26 40 a9")
-  function64(seg_start, seg_end, "_DERParseBoolean", "08 01 40 39  1f fd 03 71")
-  function64(seg_start, seg_end, "_DERParseInteger", "00 01 00 35  e8 07 40 f9")
-  function64(seg_start, seg_end, "_DERParseSequence", "e0 01 00 35  e8 07 40 f9")
-  function64(seg_start, seg_end, "_DERDecodeSeqNext", "e8 03 00 f9  28 01 08 cb")
-  function64(seg_start, seg_end, "_DERParseBitString", "08 00 80 d2  5f 00 00 39")
-  function64(seg_start, seg_end, "_DERImg4DecodePayload", "33 03 00 b4  09 01 40 f9")
-  function64(seg_start, seg_end, "_DERImg4DecodeProperty", "e8 07 40 b9  08 09 43 b2")
-  function64(seg_start, seg_end, "_DERDecodeSeqContentInit", "09 04 40 f9  08 01 09 8b")
-  function64(seg_start, seg_end, "_DERImg4DecodeFindProperty", "00 00 80 52  a8 0a 43 b2")
-  function64(seg_start, seg_end, "_DERImg4DecodeFindInSequence", "60 02 80 3d  fd 7b 44 a9")
-  function64(seg_start, seg_end, "_DERDecodeItemPartialBufferGetLength", "09 04 40 f9  3f 09 00 f1")
-  function64(seg_start, seg_end, "_DERImg4DecodeParseManifestProperties", "80 02 80 3d  a1 3a 00 91")
+  func64(seg_start, seg_end, "_DEROidCompare", "a1 01 00 b4  02 05 40 f9")
+  func64(seg_start, seg_end, "_DERImg4Decode", "61 03 00 54  88 26 40 a9")
+  func64(seg_start, seg_end, "_DERParseBoolean", "08 01 40 39  1f fd 03 71")
+  func64(seg_start, seg_end, "_DERParseInteger", "00 01 00 35  e8 07 40 f9")
+  func64(seg_start, seg_end, "_DERParseSequence", "e0 01 00 35  e8 07 40 f9")
+  func64(seg_start, seg_end, "_DERDecodeSeqNext", "e8 03 00 f9  28 01 08 cb")
+  func64(seg_start, seg_end, "_DERParseBitString", "08 00 80 d2  5f 00 00 39")
+  func64(seg_start, seg_end, "_DERImg4DecodePayload", "33 03 00 b4  09 01 40 f9")
+  func64(seg_start, seg_end, "_DERImg4DecodeProperty", "e8 07 40 b9  08 09 43 b2")
+  func64(seg_start, seg_end, "_DERDecodeSeqContentInit", "09 04 40 f9  08 01 09 8b")
+  func64(seg_start, seg_end, "_DERImg4DecodeFindProperty", "00 00 80 52  a8 0a 43 b2")
+  func64(seg_start, seg_end, "_DERImg4DecodeFindInSequence", "60 02 80 3d  fd 7b 44 a9")
+  func64(seg_start, seg_end, "_DERDecodeItemPartialBufferGetLength", "09 04 40 f9  3f 09 00 f1")
+  func64(seg_start, seg_end, "_DERImg4DecodeParseManifestProperties", "80 02 80 3d  a1 3a 00 91")
 
-  function64(seg_start, seg_end, "_Img4DecodeEvaluateDictionaryProperties", "e0 03 1f 32  0a fd 7e d3")
-  function64(seg_start, seg_end, "_Img4DecodeGetPropertyBoolean", "21 08 43 b2  e0 03 00 91")
-  function64(seg_start, seg_end, "_Img4DecodeCopyPayloadDigest", "?? ?? 02 91  e0 03 15 aa")
-  function64(seg_start, seg_end, "_Img4DecodeGetPropertyData", "00 00 80 52  e8 17 40 f9")
-  function64(seg_start, seg_end, "_Img4DecodeGetPayload", "00 81 c9 3c  20 00 80 3d")
-  function64(seg_start, seg_end, "_Img4DecodeInit", "20 01 00 35  c0 c2 00 91")
+  func64(seg_start, seg_end, "_Img4DecodeEvaluateDictionaryProperties", "e0 03 1f 32  0a fd 7e d3")
+  func64(seg_start, seg_end, "_Img4DecodeGetPropertyBoolean", "21 08 43 b2  e0 03 00 91")
+  func64(seg_start, seg_end, "_Img4DecodeCopyPayloadDigest", "?? ?? 02 91  e0 03 15 aa")
+  func64(seg_start, seg_end, "_Img4DecodeGetPropertyData", "00 00 80 52  e8 17 40 f9")
+  func64(seg_start, seg_end, "_Img4DecodeGetPayload", "00 81 c9 3c  20 00 80 3d")
+  func64(seg_start, seg_end, "_Img4DecodeInit", "20 01 00 35  c0 c2 00 91")
     
-  function64(seg_start, seg_end, "_ccn_n", "63 04 00 91  5f 00 00 f1")
-  function64(seg_start, seg_end, "_ccn_cmp", "7f 00 05 eb  c0 80 80 9a")
-  function64(seg_start, seg_end, "_ccn_sub", "84 00 04 eb  40 00 00 b5")
-  function64(seg_start, seg_end, "_ccn_add", "84 00 00 b1  40 00 00 b5")
-  function64(seg_start, seg_end, "_cc_muxp", "08 c1 20 cb  28 00 08 8a")
-  function64(seg_start, seg_end, "_cchmac_init", "69 22 00 91  8a 0b 80 52")
-  function64(seg_start, seg_end, "_ccdigest_init", "f4 03 00 aa  60 22 00 91")
-  function64(seg_start, seg_end, "_ccdigest_update", "e1 00 00 54  81 fe 46 d3")
+  func64(seg_start, seg_end, "_ccn_n", "63 04 00 91  5f 00 00 f1")
+  func64(seg_start, seg_end, "_ccn_cmp", "7f 00 05 eb  c0 80 80 9a")
+  func64(seg_start, seg_end, "_ccn_sub", "84 00 04 eb  40 00 00 b5")
+  func64(seg_start, seg_end, "_ccn_add", "84 00 00 b1  40 00 00 b5")
+  func64(seg_start, seg_end, "_cc_muxp", "08 c1 20 cb  28 00 08 8a")
+  func64(seg_start, seg_end, "_cchmac_init", "69 22 00 91  8a 0b 80 52")
+  func64(seg_start, seg_end, "_ccdigest_init", "f4 03 00 aa  60 22 00 91")
+  func64(seg_start, seg_end, "_ccdigest_update", "e1 00 00 54  81 fe 46 d3")
 
-  function64(seg_start, seg_end, "_verify_chain_signatures", "?? 09 00 b4  68 12 40 f9")
-  function64(seg_start, seg_end, "_read_counter_py_reg_el0", "20 e0 3b d5")
-  function64(seg_start, seg_end, "_write_ktrr_unknown_el1", "a0 f2 1c d5")
-  function64(seg_start, seg_end, "_boot_check_panic", "49 00 c0 d2  09 21 a8 f2")
-  function64(seg_start, seg_end, "_verify_pkcs1_sig", "68 0e 00 54  a1 12 40 f9")
-  function64(seg_start, seg_end, "_parse_extensions", "e9 23 00 91  35 81 00 91")
-  function64(seg_start, seg_end, "_read_ctrr_lock", "40 f2 3c d5")
-  function64(seg_start, seg_end, "_reload_cache", "1f 87 08 d5")
-  function64(seg_start, seg_end, "_parse_chain", "5a 3d 00 12  77 3d 00 12")
-  function64(seg_start, seg_end, "_memset", "21 1c 40 92  e3 c3 00 b2")
-  function64(seg_start, seg_end, "_memcpy", "63 80 00 91  63 e8 7b 92")
-  function64(seg_start, seg_end, "_bzero", "63 e4 7a 92  42 00 00 8b")
-  function64(seg_start, seg_end, "_panic", "e8 03 00 91  16 81 00 91") # doubt
+  func64(seg_start, seg_end, "_verify_chain_signatures", "?? 09 00 b4  68 12 40 f9")
+  func64(seg_start, seg_end, "_read_counter_py_reg_el0", "20 e0 3b d5")
+  func64(seg_start, seg_end, "_write_ktrr_unknown_el1", "a0 f2 1c d5")
+  func64(seg_start, seg_end, "_boot_check_panic", "49 00 c0 d2  09 21 a8 f2")
+  func64(seg_start, seg_end, "_verify_pkcs1_sig", "68 0e 00 54  a1 12 40 f9")
+  func64(seg_start, seg_end, "_parse_extensions", "e9 23 00 91  35 81 00 91")
+  func64(seg_start, seg_end, "_read_ctrr_lock", "40 f2 3c d5")
+  func64(seg_start, seg_end, "_reload_cache", "1f 87 08 d5")
+  func64(seg_start, seg_end, "_parse_chain", "5a 3d 00 12  77 3d 00 12")
+  func64(seg_start, seg_end, "_memset", "21 1c 40 92  e3 c3 00 b2")
+  func64(seg_start, seg_end, "_memcpy", "63 80 00 91  63 e8 7b 92")
+  func64(seg_start, seg_end, "_bzero", "63 e4 7a 92  42 00 00 8b")
+  func64(seg_start, seg_end, "_panic", "e8 03 00 91  16 81 00 91") # doubt
 
 def accept_file(fd, fname):
   ret = 0
@@ -155,7 +155,9 @@ def load_file(fd, flags, format):
         ea = ea - 0x2
 
         if (ea % 0x4) == 0 and ida_bytes.get_full_flags(ea) < 0x200:
-          # print("[sephelper]: function added = 0x%x" % ea)
+          if segbit == 1:
+            print("[sephelper]: function added = 0x%x" % ea)
+
           ida_funcs.add_func(ea)
 
         ea = ea + 0x4
